40634b660b3b4746b2eafda5061052c333fc515c
Cevdet Karakulak <cevdetkarakulak_83@hotmail.com>
2025-09-22 21:00:02 +0300

    .

---
+++ Frontends/UdemyCarBook.WebUI/Areas/Admin/Controllers/AdminCommentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.CommentDtos;
+
+namespace UdemyCarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminComment")]
+    public class AdminCommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminCommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            ViewBag.v = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7124/api/Comments/CommentListByBlog?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [Route("RemoveComment/{id}/{blogId}")]
+        public async Task<IActionResult> RemoveComment(int id, int blogId)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7124/api/Comments?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // Silme işlemi başarılıysa aynı blogun yorum listesine yönlendir
+                return RedirectToAction("Index", new { id = blogId });
+            }
+            return View();
+        }
+    }
+}
